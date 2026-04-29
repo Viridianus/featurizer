3c74470f9d9e28ef7f411f576a7bd81a44cc5c28
Dmitrii Zelenskii <dz-zd@mail.ru>
2026-04-29 07:31:17 +0300

    Create setup.py

---
+++ code/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Featurizer",
+    version="0.1.0",
+    author="Dmitrii Zelenskii",
+    author_email="dz-zd@mail.ru",
+    description="A slight modification of Connor Mayer and Robert Daland's code that learns features from segment classes.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Viridianus/Featurizer",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: BSD 3-Clause License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8.2",
+)
